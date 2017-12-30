77e9fc6d8cefc12e9698c451ac436bf71de4c0ec
Julita Lutostańska <julita.lutostanska@gmail.com>
2017-12-30 16:06:55 +0100

    Add array verification in scripts

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 var prefix = "https://cors-anywhere.herokuapp.com/";
 
+
 function getQuote() {
     $.getJSON(prefix + quoteUrl, createTweet);
     $.ajaxSetup({ cache: false });
 }
-
+ 
 function createTweet(input) {
     var data = input[0];
+
+    if ($.isArray(data) && !data.length === 0) {
+    return;
+    }
 
     var quoteText = $(data.content).text().trim();
     var quoteAuthor = data.title;
@@ -21,23 +26,22 @@ function createTweet(input) {
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 
     if (tweetText.length > 140) {
-    getQuote();
-	} else {
-    var tweet = tweetLink + encodeURIComponent(tweetText);
-    $('.quote').text(quoteText);
-    $('.author').text("Author: " + quoteAuthor);
-    $('.tweet').attr('href', tweet);
-	}
-
-	$('.tweet').attr('href', tweet);
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text("Author: " + quoteAuthor);
+        $('.tweet').attr('href', tweet);
+    }       
 }
 
 $(document).ready(function() {
     getQuote();
     $('.trigger').on('click', function() {
         getQuote();
-    })
+    });
 });
+
 
 
 });
